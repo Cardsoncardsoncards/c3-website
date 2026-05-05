f7c60fa1b7eb833e51ecd8fd5092919f517d7409
Sammy G <ccc.squadhelp@gmail.com>
2026-05-06 06:44:31 +1000

    fix: card vault link, sitemap-cards passthrough, delete root tracker

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/c3-logo.png": "c3-logo.png"});
   eleventyConfig.addPassthroughCopy({"sitemap.xml": "sitemap.xml"});
   eleventyConfig.addPassthroughCopy({"sitemap-index.xml": "sitemap-index.xml"});
+  eleventyConfig.addPassthroughCopy({"sitemap-cards.xml": "sitemap-cards.xml"});
   eleventyConfig.addPassthroughCopy({"ev-calculator": "ev-calculator"});
   eleventyConfig.addPassthroughCopy({"netlify": "netlify"});
   eleventyConfig.addPassthroughCopy({"netlify.toml": "netlify.toml"});
